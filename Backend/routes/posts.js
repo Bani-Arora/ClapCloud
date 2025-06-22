9cc7bcc8d123f1b64d12068a80b457141e12649e
Bani <100341485+Bani-Arora@users.noreply.github.com>
2025-06-23 00:57:18 +0530

    Update posts.js

--- Backend/routes/posts.js
+++ Backend/routes/posts.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import auth from '../middleware/auth.js'
+import auth from '../middleware/Auth.js'
 import Post from '../models/Post.js'
 import upload from '../middleware/upload.js'
 
